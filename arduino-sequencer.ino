56f1b6664b9679746a594bd29a0e908ac1cae128
Jørn Holm Aske <brotulix@gmail.com>
2023-07-02 15:07:22 +0200

    First beginnings of a program skeleton.

--- arduino-sequencer.ino
+++ arduino-sequencer.ino
@@ -1,5 +1,50 @@
 #define TEST 1
 
+typedef enum
+{
+    STATE_UNINITIALIZED,
+    STATE_RX,
+    STATE_TRANSITION,
+    STATE_TX,
+    STATE_NUM_STATES
+} states_t;
+
+states_t state = STATE_UNINITIALIZED;
+
+typedef enum
+{
+    LEVEL_UNINITIALIZED,
+    LEVEL_RX,
+    LEVEL_PREAMP,
+    LEVEL_COAX_RELAY_A,
+    LEVEL_COAX_RELAY_B,
+    LEVEL_PA_PTT,
+    LEVEL_TX_INHIBIT,
+    LEVEL_TX,
+    LEVEL_NUM_LEVELS
+} levels_t;
+
+levels_t level = LEVEL_UNINITIALIZED;
+
+
+
+void Transition(bool to_tx)
+{
+    // We move "up" in levels towards TX and "down" towards RX
+    char level_modifier = to_tx ? 1 : -1;
+
+    
+}
+
+void Initialize()
+{
+
+
+}
+
+
+
+
 void setup()
 {
     Serial.begin(115200);
@@ -9,5 +54,6 @@ void setup()
 
 void loop()
 {
+    millis();
 
 }
