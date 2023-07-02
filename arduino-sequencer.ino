56952c41611215be6f192318b79816f8460e2105
Jørn Holm Aske <brotulix@gmail.com>
2023-07-02 15:52:28 +0200

    Baby steps. VS Code Arduino extension apparently doesn't play well with remote sessions, so have to resort to traditional SSH/X.

--- arduino-sequencer.ino
+++ arduino-sequencer.ino
@@ -1,5 +1,14 @@
 #define TEST 1
 
+// @ Todo: maybe use a mega instead for more digital I/O during development?
+const int pin_preamp = 2;
+const int pin_coax_relay_a = 3;
+const int pin_coax_relay_b = 4;
+const int pin_pa_ptt = 5;
+const int pin_tx_inhibit = 6;
+const int pin_rx = 7;
+const int pin_tx = 9;
+
 typedef enum
 {
     STATE_UNINITIALIZED,
@@ -26,18 +35,37 @@ typedef enum
 
 levels_t level = LEVEL_UNINITIALIZED;
 
-
+unsigned long delays_ms[LEVEL_NUM_LEVELS] =
+{
+    0,      // LEVEL_UNINITIALIZED
+    0,      // LEVEL_RX
+    1000,   // LEVEL_PREAMP
+    1000,   // LEVEL_COAX_RELAY_A
+    1000,   // LEVEL_COAX_RELAY_B
+    1000,   // LEVEL_PA_PTT
+    1000,   // LEVEL_TX_INHIBIT
+    0       // LEVEL_TX
+            // LEVEL_NUM_LEVELS
+};
 
 void Transition(bool to_tx)
 {
     // We move "up" in levels towards TX and "down" towards RX
     char level_modifier = to_tx ? 1 : -1;
+    pinMode(pin_tx_inhibit, OUTPUT);
+    digitalWrite(pin_tx_inhibit, 1);
 
+    delay(delays_ms[LEVEL_TX_INHIBIT]);
+
+    
     
 }
 
 void Initialize()
 {
+
+    // Set each pin as output and to a safe-ish state, then set state to RX.
+
 
 
 }
